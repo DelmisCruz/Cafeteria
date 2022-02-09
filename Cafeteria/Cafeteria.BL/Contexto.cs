a3a8f7696e1320f97ab76c5b018539a6ba23043f
Delmis Cruz Rosa <delmismarisela17@gmail.com>
2022-02-09 15:23:56 -0600

    Avance hasta el video 12

---
+++ Cafeteria/Cafeteria.BL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafeteria.BL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto() : base("CafeteriaBD")
+        {
+
+        }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
--- Cafeteria/Cafeteria.BL/ProductosBL.cs
+++ Cafeteria/Cafeteria.BL/ProductosBL.cs
@@ -8,29 +8,16 @@ namespace Cafeteria.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Americano";
-            producto1.Precio = 45;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Latte";
-            producto2.Precio = 48;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Mocha";
-            producto3.Precio = 50;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
+            return _contexto.Productos.ToList();
         }
     }
 }
