9452a69c6e4081059eace61adede34d1428f3143
Delmis Cruz Rosa <delmismarisela17@gmail.com>
2022-02-09 14:34:45 -0600

    Agregando Arquitectura Basica

--- Cafeteria/Cafeteria.Web/Controllers/ProductosController.cs
+++ Cafeteria/Cafeteria.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using Cafeteria.Web.Models;
+﻿using Cafeteria.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +12,8 @@ namespace Cafeteria.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Americano";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Latte";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Mocha";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
